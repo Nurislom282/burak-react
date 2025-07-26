f8c825c959fe7dd7c2c209ccaf232fa396bfe40a
Nurislom <anurislom425@gmail.com>
2025-07-26 13:28:31 +0500

    feat: publish otherNavbar header component

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,62 @@
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar() {
-  return <div>OtherNavbar</div>;
+  const authmember = null;
+  return (
+    <div className="other-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to="/">
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className={"hover-line"}>
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink to="/products" activeClassName={"underline"}>
+                Products
+              </NavLink>
+            </Box>
+            {authmember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/orders" activeClassName={"underline"}>
+                  Orders
+                </NavLink>
+              </Box>
+            ) : null}
+            {authmember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page" activeClassName={"underline"}>
+                  My Page
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"hover-line"}>
+              <NavLink to="/help" activeClassName={"underline"}>
+                Help
+              </NavLink>
+            </Box>
+            {/* BASKET */}
+            {!authmember ? (
+              <Box>
+                <Button variant="contained" className="login-button">
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src={"/icons/default-user.svg"}
+                aria-haspopup={"true"}
+              />
+            )}
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
