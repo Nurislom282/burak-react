6a09b48cc5a0fa0eb24566867a4213b529a37bfd
Nurislom <anurislom425@gmail.com>
2025-07-27 23:02:23 +0500

    fix: modify statistics sctional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,6 +5,7 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advaertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
+import "../../../css/home.css";
 
 export default function Homepage() {
   return (
