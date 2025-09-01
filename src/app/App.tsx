bb014a3282ba939a7cbdf07fc5e73de99b86b18d
Nurislom282 <anurislom425@gmail.com>
2025-09-01 16:57:49 +0500

    fix: modify basket business logic vis customized hooks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,63 +15,49 @@ import "../css/navbar.css";
 import "../css/footer.css";
 import Test from "./screens/Test";
 import { CartItem } from "../lib/types/search";
+import useBasket from "./hooks/useBasket";
 
 function App() {
   const location = useLocation();
-
-  const cartJson: string | null = localStorage.getItem("cartData");
-  const currentCart = cartJson ? JSON.parse(cartJson) : [];
-  const [cartItems, setCartItems] = useState<CartItem[]>(currentCart);
-
-  /* HANDLERS */
-  const onAdd = (input: CartItem) => {
-    const exist: any = cartItems.find(
-      (item: CartItem) => item._id === input._id
-    );
-    if (exist) {
-      const cartUpdate = cartItems.map((item: CartItem) =>
-        item._id === input._id
-          ? { ...exist, quantity: exist.quantity + 1 }
-          : item
-      );
-      setCartItems(cartUpdate);
-      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    } else {
-      const cartUpdate = [...cartItems, { ...input }];
-      setCartItems(cartUpdate);
-      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
-    }
-  };
-
+  const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
   return (
-    console.log(cartItems),
-    (
-      <>
-        {location.pathname === "/" ? (
-          <HomeNavbar cartItems={cartItems} />
-        ) : (
-          <OtherNavbar cartItems={cartItems} />
-        )}
-        <Switch>
-          <Route path="/products">
-            <ProductsPage onAdd={onAdd} />
-          </Route>
-          <Route path="/orders">
-            <OrdersPage />
-          </Route>
-          <Route path="/member-page">
-            <UserPage />
-          </Route>
-          <Route path="/help">
-            <HelpPage />
-          </Route>
-          <Route path="/">
-            <Homepage />
-          </Route>
-        </Switch>
-        <Footer />
-      </>
-    )
+    <>
+      {location.pathname === "/" ? (
+        <HomeNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+        />
+      ) : (
+        <OtherNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+        />
+      )}
+      <Switch>
+        <Route path="/products">
+          <ProductsPage onAdd={onAdd} />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -6,10 +6,14 @@ import { CartItem } from "../../../lib/types/search";
 
 interface HomeNavbarProps {
   cartItems: CartItem[];
+  onAdd: (item: CartItem) => void;
+  onRemove: (item: CartItem) => void;
+  onDelete: (item: CartItem) => void;
+  onDeleteAll: () => void;
 }
 
 export default function HomeNavbar(props: HomeNavbarProps) {
-  const { cartItems } = props;
+  const { cartItems, onAdd, onDelete, onRemove, onDeleteAll } = props;
   const authmember = true;
 
   const [count, setCount] = useState<number>(0);
@@ -67,7 +71,13 @@ export default function HomeNavbar(props: HomeNavbarProps) {
                 Help
               </NavLink>
             </Box>
-            <Basket cartItem={cartItems} />
+            <Basket
+              cartItem={cartItems}
+              onAdd={onAdd}
+              onRemove={onRemove}
+              onDelete={onDelete}
+              onDeleteAll={onDeleteAll}
+            />
             {!authmember ? (
               <Box>
                 <Button variant="contained" className="login-button">
